6627ccc3d5c434a2135dfcf8a80dbcd3e3f9b9dc
mrizkirohmat <mrizkirohmatp4k@gmail.com>
2022-06-15 18:08:37 +0700

    commit 04

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,28 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fncallback) => {
+  fs.readFile(file1, "utf-8", (err, data1) => {
+    if (err) return fncallback(err);
+    fs.readFile(file2, "utf-8", (err, data2) => {
+      if (err) return fncallback(err);
+      fs.readFile(file3, "utf-8", (err, data3) => {
+        let result =
+          JSON.parse(data1).message +
+          " " +
+          JSON.parse(data2)[0].message +
+          " " +
+          JSON.parse(data3)[0].data.message;
+        fncallback(
+          err,
+          result
+            .split(" ")
+            .filter((element, index) => element.toLowerCase() !== "halo")
+        );
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
